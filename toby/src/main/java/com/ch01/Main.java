b18eb3ca2c84e1f8edbbbbeb6638d54d7e1439c2
yangrangrang <gkssk2309@gmail.com>
2023-10-11 22:06:24 +0900

    1-3 mainTest

---
+++ toby/src/main/java/com/ch01/Main.java
@@ -0,0 +1,27 @@
+package com.ch01;
+
+import com.ch01.dao.UserDao;
+import com.ch01.domain.User;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("test1111");
+        user.setName("yang");
+        user.setPassword("yang");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + "등록성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회성공");
+    }
+}
