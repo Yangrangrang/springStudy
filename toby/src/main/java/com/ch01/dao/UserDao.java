d115732c29bd816f2ac5d0e743a9ecd7e6487064
yangrangrang <gkssk2309@gmail.com>
2023-10-11 21:57:35 +0900

    1-2 userDao

---
+++ toby/src/main/java/com/ch01/dao/UserDao.java
@@ -0,0 +1,50 @@
+package com.ch01.dao;
+
+import com.ch01.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        // Class.forName : DB 드라이버 로드
+        Class.forName("com.mysql.jdbc.Driver");
+        // DB 연결을 위한 Connection
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPRINGSTUDYDB", "root", "root");
+
+        // SQL 담기 (prepareStatement) : users 테이블 insert문
+        PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values (?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        // select문을 제외한 다른 구문을 수행할 때 사용하는 함수 (executeUpdate)
+        ps.executeUpdate();
+
+        // SOL prepareStatement 닫기
+        ps.close();
+        // Connection 닫기
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPRINGSTUDYDB", "root", "root");
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
