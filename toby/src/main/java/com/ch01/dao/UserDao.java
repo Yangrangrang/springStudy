13c9143875c03d74205d5ff2b9107b4305f1a0e6
yangrangrang <gkssk2309@gmail.com>
2023-10-11 22:57:10 +0900

    1-5 extends UserDao

--- toby/src/main/java/com/ch01/dao/UserDao.java
+++ toby/src/main/java/com/ch01/dao/UserDao.java
@@ -45,25 +45,6 @@ public abstract class UserDao {
 
     public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 
-    public class NUserDao extends UserDao {
 
-        @Override
-        public Connection getConnection() throws ClassNotFoundException, SQLException {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPRINGSTUDYDB", "root", "root");
-            return c;
-        }
-    }
-
-    public class DUserDao extends  UserDao {
-
-        @Override
-        public Connection getConnection() throws ClassNotFoundException, SQLException {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPRINGSTUDYDB", "root", "root");
-            return c;
-        }
-    }
 }
+
