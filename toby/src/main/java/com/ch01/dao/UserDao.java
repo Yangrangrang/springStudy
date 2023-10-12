38e80ad6b74e1e686c2ccfd5eb3f2bb617e7f7a2
yangrangrang <gkssk2309@gmail.com>
2023-10-12 14:59:37 +0900

    1-10

--- toby/src/main/java/com/ch01/dao/UserDao.java
+++ toby/src/main/java/com/ch01/dao/UserDao.java
@@ -5,13 +5,13 @@ import com.ch01.domain.User;
 import java.sql.*;
 
 public class UserDao {
-    private SimpleConnectionMaker simpleConnectionMaker;
+    private ConnectionMaker connectionMaker;
 
     public UserDao(){
-        simpleConnectionMaker = new SimpleConnectionMaker();
+        connectionMaker = new DConnectionMaker();
     }
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
 
         // SQL 담기 (prepareStatement) : users 테이블 insert문
         PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values (?,?,?)");
@@ -29,7 +29,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
