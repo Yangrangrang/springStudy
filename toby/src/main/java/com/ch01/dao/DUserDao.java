81ccf801c15dbd73a20bf30fcd665b772d9f0f19
yangrangrang <gkssk2309@gmail.com>
2023-10-12 15:45:31 +0900

    1-11

--- toby/src/main/java/com/ch01/dao/DUserDao.java
+++ toby/src/main/java/com/ch01/dao/DUserDao.java
@@ -4,13 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DUserDao extends UserDao {
-
-    @Override
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPRINGSTUDYDB", "root", "root");
-        return c;
-    }
-}
+//public class DUserDao extends UserDao {
+//
+//    @Override
+//    public Connection getConnection() throws ClassNotFoundException, SQLException {
+//        Class.forName("com.mysql.jdbc.Driver");
+//
+//        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPRINGSTUDYDB", "root", "root");
+//        return c;
+//    }
+//}
