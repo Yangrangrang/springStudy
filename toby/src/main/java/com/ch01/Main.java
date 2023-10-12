536546c60b6fafb0f4663635ddeef16a98ff0909
yangrangrang <gkssk2309@gmail.com>
2023-10-12 15:47:07 +0900

    1-12

--- toby/src/main/java/com/ch01/Main.java
+++ toby/src/main/java/com/ch01/Main.java
@@ -1,5 +1,7 @@
 package toby.src.main.java.com.ch01;
 
+import com.ch01.dao.ConnectionMaker;
+import com.ch01.dao.DConnectionMaker;
 import com.ch01.dao.UserDao;
 import com.ch01.domain.User;
 
@@ -7,18 +9,20 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao userDao = new UserDao();
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+
+        UserDao dao = new UserDao(connectionMaker);
 
         User user = new User();
         user.setId("test1113");
         user.setName("yang");
         user.setPassword("yang");
 
-        userDao.add(user);
+        dao.add(user);
 
         System.out.println(user.getId() + "등록성공");
 
-        User user2 = userDao.get(user.getId());
+        User user2 = dao.get(user.getId());
         System.out.println(user2.getName());
         System.out.println(user2.getPassword());
 
